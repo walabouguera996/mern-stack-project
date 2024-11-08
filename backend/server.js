9d5269fb25b8c5125edecded60f516c8c8a97c0a
walabouguera996 <walabouguera996@gmail.com>
2024-11-08 15:04:18 +0100

    mongobd

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,32 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
-const app = express ()
+const app = express()
 
-//middleware
+// middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
-//routes
-app.get('/', (req, res)=>{
-    res.json({messg: 'welcom to the app'})
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+// listen for requests
+    app.listen(process.env.PORT, () => {
+        console.log('connected to db & listening on port', process.env.PORT)
+    })
+})
+.catch((error) => {
+    console.log(error)
 })
 
-// listen for request
-app.listen(process.env.PORT, () => {
-    console.log('listeneing on port', process.env.PORT)
-})
