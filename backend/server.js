f119e0ee8178adc4a229ff4a13cdc2e52198f5e0
walabouguera996 <walabouguera996@gmail.com>
2024-11-10 01:18:35 +0100

    last up date

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,8 @@ const app = express()
 app.use(express.json())
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
+  console.log(req.path, req.method)
+  next()
 })
 
 // routes
@@ -20,13 +20,13 @@ app.use('/api/workouts', workoutRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-// listen for requests
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
     app.listen(process.env.PORT, () => {
-        console.log('connected to db & listening on port', process.env.PORT)
+      console.log('listening for requests on port', process.env.PORT)
     })
-})
-.catch((error) => {
-    console.log(error)
-})
-
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
